4915360e45fa063a65cc8d60be42500b8fee3cba
felipe <85032728+Flipe124@users.noreply.github.com>
2022-09-29 20:32:55 -0300

    att

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("teste");
+
+        Hashtable<String, ArrayList<String>> contato = new Hashtable<String, ArrayList<String>>();
+
+    }
+}
