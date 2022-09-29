61a2cd310af48c55d4b421ef1002d37b1aa8825b
felipe <85032728+Flipe124@users.noreply.github.com>
2022-09-29 20:50:51 -0300

    finalizado

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,38 @@ public class Main {
     public static void main(String[] args){
         System.out.println("teste");
 
-        Hashtable<String, ArrayList<String>> contato = new Hashtable<String, ArrayList<String>>();
+        Hashtable<String, ArrayList<String>> tabela = new Hashtable<String, ArrayList<String>>();
 
+        ArrayList<String> felipe = new ArrayList<String>();
+
+        felipe.add("Bob");
+        felipe.add("Claire");
+        felipe.add("Alice");
+
+        ArrayList<String> bob = new ArrayList<String>();
+
+        bob.add("Anuj");
+        bob.add("Peggy");
+
+        ArrayList<String> claire = new ArrayList<String>();
+
+        claire.add("Thom");
+        claire.add("Jonny");
+
+        ArrayList<String> alice = new ArrayList<String>();
+
+        alice.add("Peggy");
+
+        tabela.put("Felipe", felipe);
+        tabela.put("Bob", bob);
+        tabela.put("Claire", claire);
+        tabela.put("Alice", alice);
+
+        for (Map.Entry<String, ArrayList<String>> entry : tabela.entrySet()){
+            String chave = entry.getKey();
+            ArrayList<String> contatos = entry.getValue();
+
+            System.out.println("Pessoa: " + chave + " - Contatos: " + contatos);
+        }
     }
 }
